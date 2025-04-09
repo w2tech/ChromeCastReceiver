b8033354e07fe3a2778269bcf803a778125c97e5
w2tech <53912406+w2tech@users.noreply.github.com>
2025-04-09 23:53:39 +0200

    Create dhcpd.py

---
+++ src/dhcpd.py
@@ -0,0 +1,20 @@
+class Dhcpd():
+    """DHCP server daemon running in background."""
+
+    def __init__(self, interface):
+        """Constructor accept an interface to listen."""
+        self.dhcpd = None
+        self.interface = interface
+
+    def start(self):
+        fd, self.conf_path = tempfile.mkstemp(suffix='.conf')
+        conf = "start  {}\nend {}\ninterface {}\noption subnet {}\noption lease {}\n".format(
+            Settings.peeraddress, Settings.peeraddress, self.interface, Settings.netmask, Settings.timeout)
+        with open(self.conf_path, 'w') as c:
+            c.write(conf)
+        self.dhcpd = subprocess.Popen(["sudo", "udhcpd", self.conf_path])
+
+    def stop(self):
+        if self.dhcpd is not None:
+            self.dhcpd.terminate()
+            self.conf_path.unlink()
